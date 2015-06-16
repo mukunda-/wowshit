ad58e4fd0ebb390ab797f1f834e2d75f923561ac
mukunda- <mukunda@mukunda.com>
2015-06-16 17:21:21 -0600

    a

--- poker/RPPoker/RPPoker.lua
+++ poker/RPPoker/RPPoker.lua
@@ -9,11 +9,28 @@ local g_dealer -- player index of current dealer
 local BUTTON_ICON = 2 -- dealer icon (circle)
 local TURN_ICON   = 4 -- turn icon (triangle)
 
-local g_ante = 250
+local g_ante        = 250
 local g_small_blind = 500
-local g_big_blind = 1000
+local g_big_blind   = 1000
+local g_multiplier  = 1.0
 
 local g_deck = {}
+
+
+local STATE_LIVE  = 2 -- playing a game
+
+local g_betting_round
+-- 1 = preflop
+-- 2 = postflop
+-- 3 = postturn
+-- 4 = postriver
+-- 5 = showdown
+
+local g_flopped -- has the flop been dealt
+local g_turned  -- has the turn been dealt
+
+-- game state
+local g_state = STATE_SETUP
 
 -------------------------------------------------------------------------------
 -- Shuffles a new deck.
@@ -47,8 +64,9 @@ end
 -------------------------------------------------------------------------------
 local function CreatePlayer( name, credit )
 	return {
-		name = name
-		credit = credit
+		name   = name;
+		credit = credit;
+		hand   = {}; -- aka hole
 	}
 end
 
@@ -79,10 +97,47 @@ local function StartGame()
 end
 
 -------------------------------------------------------------------------------
-local function DealHand()
-	-- start a new hand.
+-- Tell a player what cards they have
+--
+-- @param player Player data.
+--
+local function TellCards( player )
 	
+	local msg = string.format( "Your cards: %s, %s",
+							   CardName( p.hand[1] ), 
+							   CardName( p.hand[2] ))
+	
+	SendChatMessage( msg, "WHISPER", _, player.name )
+end
+
+-------------------------------------------------------------------------------
+-- Start a new hand.
+--
+local function DealHand()
+	
+	-- emote: deals a new hand. <a> and <b> place the blinds (1000g)
 	SendChatMessage( "deals a new hand.", "EMOTE" )
+	
+	for k,v in pairs( g_players ) do
+		v.folded = false
+		v.hand = {}
+	end
+	
+	local x = g_dealer
+	
+	for i = 1, #g_players*2 do
+		x = x + 1
+		if x > #g_players then
+			x = 1
+		end
+		
+		table.insert( g_players[i].hand, DrawCard )
+	end
+	
+	for k,v in pairs( g_players ) do
+		TellCards( v )
+	end
+	
 	
 	
 end
