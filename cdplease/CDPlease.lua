d5bd40e58f4ea31e29be7cbf145e59e962f09e21
mukunda- <mukunda@mukunda.com>
2015-07-23 09:45:36 -0500

    a

--- cdplease/CDPlease.lua
+++ cdplease/CDPlease.lua
@@ -4,5 +4,22 @@
 -- psuedo protocol:
 --
 -- /cdplease:
---   
+--   send query to raid
+--   gather responses within threshold (250ms)
+--   request 
 
+local cd_call_time = 0
+
+SLASH_CDPLEASE = "/cdplease"
+
+function SLASHCMDLIST.CDPLEASE()
+	CallCD()
+end
+
+local function CallCD()
+	if GetTime() - cd_call_time < 1 then
+		return -- ignore button spam
+	end
+	
+	
+end
