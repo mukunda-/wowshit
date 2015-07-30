976a60fbddbf3a55d6498f30ad61c87232dae0ac
mukunda- <mukunda@mukunda.com>
2015-07-30 10:07:16 -0500

    a

--- cdplease/CDPlease.lua
+++ cdplease/CDPlease.lua
@@ -4,15 +4,13 @@
 -- psuedo protocol:
 --
 -- /cdplease:
---   send query to raid
---   gather responses within threshold (250ms)
---   request 
---
+--   send "ASK" to raid
+--   raid responds with cds available
+--   if a cd is within range with 10 extra yards, request that cd immediately
+--   timeout after threshold 250ms and select a cd that is the least out of range
+--   give up after 1000ms if no cds are received
 
-local MyAddon = LibStub("AceAddon-3.0"):NewAddon("CDPlease")
-
-
-
+local MyAddon      = LibStub("AceAddon-3.0"):NewAddon("CDPlease") 
 local cd_call_time = 0
 
 SLASH_CDPLEASE = "/cdplease"
@@ -30,6 +28,7 @@ local function CallCD()
 	cd_call_time = GetTime()
 	
 	SendAddonMessage( "CDPLEASE", "ASK", "RAID" )
+	
 	
 	
 end
